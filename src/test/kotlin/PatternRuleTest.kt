2dc701bbc5160771c730a079bd5c016e55e97ba8
Bouke Versteegh <info@boukeversteegh.nl>
2019-02-06 14:45:22 +0100

    Testing works through gradle and RunConfiguration, but include in browser doesn't

--- src/test/kotlin/PatternRuleTest.kt
+++ src/test/kotlin/PatternRuleTest.kt
@@ -1,10 +1,24 @@
+import be.anagon.cells.CustomPatternRule
+import be.anagon.cells.Dirt
+import be.anagon.cells.None
+import be.anagon.cells.Position
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class PatternRuleTest {
 
     @Test
-    fun relativeTo() {
-        assertEquals(4, 2 * 2)
+    fun evaluate() {
+        val rule = CustomPatternRule(
+            mapOf(
+                Position(0, 0) to None
+            ), mapOf(
+                Position(0, 0) to Dirt
+            )
+        )
+
+        val output = rule.evaluate(Position(10, 10), mapOf(Position(10, 10) to None))
+
+        assertEquals(mapOf(Position(10, 10) to Dirt), output)
     }
 }
